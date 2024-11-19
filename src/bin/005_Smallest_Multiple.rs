ad531460af469a8374ccf2fd7215d24fbe960f64
Christopher <ckaliszewski@gmail.com>
2024-11-19 16:45:59 -0500

    add problem 5's solution

---
+++ src/bin/005_Smallest_Multiple.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+
+/// Solution to Problem 5: [Smallest Multiple](https://projecteuler.net/problem=5)
+///
+/// Returns smallest number that is evenly divisible from 1 to below given threshold.
+#[derive(Parser)]
+struct Args {
+    /// Threshold of solution.
+    #[arg[short, long, default_value_t = 20]]
+    threshold: u32,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Answer: {}", solution(args.threshold));
+}
+
+fn solution(threshold: u32) -> u32 {
+    (1..)
+        .find(|i| (1..=threshold).all(|j| i % j == 0))
+        .expect("failed to find a solution")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solution_returns_expected_answer() {
+        // assert
+        assert_eq!(2520, solution(10));
+    }
+}
